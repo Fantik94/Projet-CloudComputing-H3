369d499eb5a4736056a447cf7a1e1ada91a55c3b
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-01-17 15:36:48 +0100

    appli trop bien --> api+front+css

--- backend-node/app.js
+++ backend-node/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mysql = require('mysql2');
 const fs = require('fs');
+const cors = require('cors')
 
 // Création de l'application Express
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Configuration de la connexion MySQL
 const connection = mysql.createConnection({
@@ -28,7 +30,7 @@ app.get('/test-db', (req, res) => {
   });
 });
 
-app.get('/blogs', (req, res) => {
+app.get('/all-blogs', (req, res) => {
     connection.query('SELECT * FROM blog', (err, results) => {
       if (err) {
         return res.status(500).send('Erreur lors de la récupération des données : ' + err.message);
@@ -37,6 +39,34 @@ app.get('/blogs', (req, res) => {
     });
   });
   
+
+app.post('/blogs', (req, res) => {
+  const { titre, description, image_url, pseudo } = req.body;
+  const query = 'INSERT INTO blog (titre, description, image_url, pseudo) VALUES (?, ?, ?, ?)';
+
+  connection.query(query, [titre, description, image_url, pseudo], (err, results) => {
+    if (err) {
+      return res.status(500).send('Erreur lors de l\'ajout de l\'entrée : ' + err.message);
+    }
+    res.status(201).send(`Entrée ajoutée avec l'ID: ${results.insertId}`);
+  });
+});
+
+
+app.delete('/blogs/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'DELETE FROM blog WHERE id = ?';
+  
+    connection.query(query, [id], (err, results) => {
+      if (err) {
+        return res.status(500).send('Erreur lors de la suppression de l\'entrée : ' + err.message);
+      }
+      if (results.affectedRows === 0) {
+        return res.status(404).send('Aucune entrée trouvée avec cet ID.');
+      }
+      res.send(`Entrée avec l'ID ${id} a été supprimée.`);
+    });
+  });
 
 // Démarrage du serveur
 const PORT = 3000;
