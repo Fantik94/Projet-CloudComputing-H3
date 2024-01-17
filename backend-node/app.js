2a4f513ee2623ec4673f160a303ef19d5f8da5bd
Fantik94 <84931090+Fantik94@users.noreply.github.com>
2024-01-17 14:28:59 +0100

    push

---
+++ backend-node/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql2');
+const fs = require('fs');
+
+// Création de l'application Express
+const app = express();
+app.use(express.json());
+
+// Configuration de la connexion MySQL
+const connection = mysql.createConnection({
+    host: 'damienbaptistemysqlserver.mysql.database.azure.com',
+    user: 'mysqladmin@damienbaptistemysqlserver',
+    password: 'Dadabapt94340@',
+    database: 'damienbaptistemysqldb',
+    port: 3306,
+    ssl  : {
+        ca : fs.readFileSync('DigiCertGlobalRootG2.crt.pem')
+      }
+  });
+
+// Endpoint pour tester la connexion à la base de données
+app.get('/test-db', (req, res) => {
+  connection.connect(err => {
+    if (err) {
+      return res.status(500).send('Erreur lors de la connexion à la base de données : ' + err.message);
+    }
+    res.send('Connexion à la base de données réussie !');
+  });
+});
+
+// Démarrage du serveur
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
